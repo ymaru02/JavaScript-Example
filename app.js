5e629d4003b9e7301b02606bef255deade53d676
Ymaru02 <dyun022@gmail.com>
2022-03-10 06:50:49 +0900

    router

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// let ajax = new XMLHttpRequest();
 const container = document.getElementById("root");
 const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
@@ -6,36 +5,61 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
-
-const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement("ul");
-
-window.addEventListener("hashchange", function () {
-  const id = location.hash.substr(1);
-  console.log(CONTENT_URL.replace("@id", id));
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-
+function getDate(url) {
+  ajax.open("GET", url, false);
   ajax.send();
 
-  const newsContent = JSON.parse(ajax.response);
-  const title = document.createElement("h1");
-
-  title.innerHTML = newsContent.title;
-  console.log(newsContent);
-});
-
-for (let i = 0; i < 10; i++) {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
-  li.appendChild(a);
-  ul.appendChild(li);
+  return JSON.parse(ajax.response);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsFeed() {
+  const newsFeed = getDate(NEWS_URL);
+  const newsList = [];
+
+  newsList.push("<ul>");
+
+  for (let i = 0; i < 10; i++) {
+    const div = document.createElement("div");
+
+    newsList.push(`
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `);
+  }
+
+  newsList.push("</ul>");
+  container.innerHTML = newsList.join("");
+}
+
+function newDetail() {
+  const id = location.hash.substr(1);
+
+  const newsContent = getDate(CONTENT_URL.replace("@id", id));
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+      <a href="#">
+        목록으로
+      </a>
+    </div>
+  `;
+}
+
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
