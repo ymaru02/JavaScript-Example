bf9404e6228292374e9ec088e61d3c2df96b7904
Ymaru02 <dyun022@gmail.com>
2022-03-14 04:21:59 +0900

    clock

--- NomadCoders/Chrome/js/greetings.js
+++ NomadCoders/Chrome/js/greetings.js
@@ -1,46 +1,26 @@
 const loginForm = document.querySelector("#login-form");
-
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = loginForm.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
-// const link = document.querySelector("a");
-
-// function onLoginBtnClick() {
-//   const username = loginInput.value;
-//   if (username === "") {
-//     alert("Please write your name");
-//   } else if (username.length) {
-//     alert("Your name is too long.");
-//   }
-// }
-
-// loginButton.addEventListener("click", onLoginBtnClick);
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  // greeting.innerText = "Hello " + username;
   paintGreetings(savedUserName);
 }
 
-// function handleLinkClick(event) {
-//   event.preventDefault();
-// }
-
-// link.addEventListener("click", handleLinkClick);
-
 function paintGreetings(username) {
-  greeting.classList.remove(HIDDEN_CLASSNAME);
   greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUserName = localStorage.getItem(USERNAME_KEY);
 
+console.log(savedUserName);
 if (savedUserName === null) {
   //  show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
